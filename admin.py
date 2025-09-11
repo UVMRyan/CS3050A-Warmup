620bd27538c9f8e701412c0089858a77604d17d7
mclark41 <mclark41@uvm.edu>
2025-09-11 15:56:19 -0400

    Admin.py fully functional

--- admin.py
+++ admin.py
@@ -1,5 +1,4 @@
 # Admin file 
-# DO NOT RUN FIRESTORE HAS DATA WORKING ON DELETION
 import json
 import firebase_admin
 from firebase_admin import firestore
@@ -8,15 +7,27 @@ from firebase_admin import credentials
 # Initializing the database and connection to firestore
 cred = credentials.Certificate('cs3050-warmup-891d7-firebase-adminsdk-fbsvc-48b3b532ba.json')
 firebase_admin.initialize_app(cred)
-
-# Create connection to the firestore
 firestore_conn = firestore.client()
 
+# Read data from the json file
 with open('pokemon_data.json', 'r') as pokemonFile:
     pokemonData = json.load(pokemonFile)
 
+# Start new collection in firestore 
 collectionReference = firestore_conn.collection("pokemon")
 
+# Check if the collection is populated
+docCheck = collectionReference.limit(1).get()
+
+# Docs is every document in the firestore
+docs = collectionReference.stream()
+
+# If the collection is populated delete everything
+if len(list(docCheck)) >= 1:
+    for doc in docs:
+        doc.reference.delete()
+
+# Otherwise populate the pokemon collection
 for pokemon in pokemonData:
     docID = str(pokemon["number"])
     collectionReference.document(docID).set(pokemon)
