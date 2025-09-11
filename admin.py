35f25515efaad3bead4318833314379da2d1bc6d
mclark41 <mclark41@uvm.edu>
2025-09-11 15:09:02 -0400

    Updates to admin.py

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 # Admin file 
+import json
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
@@ -6,4 +7,16 @@ from firebase_admin import credentials
 # Initializing the database and connection to firestore
 cred = credentials.Certificate('cs3050-warmup-891d7-firebase-adminsdk-fbsvc-48b3b532ba.json')
 firebase_admin.initialize_app(cred)
+
+# Create connection to the firestore
+firestore_conn = firestore.client()
+
+with open('pokemon_data.json', 'r') as pokemonFile:
+    pokemonData = json.load(pokemonFile)
+
+collectionReference = firestore_conn.collection("pokemon")
+
+for pokemon in pokemonData:
+    docID = str(pokemon["number"])
+    collectionReference.document(docID).set(pokemon)
 
